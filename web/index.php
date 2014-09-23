2d6acacc7803f02624a2660f9802fa43a38b0c68
Andi Susanto <andi.susanto@sofialys.com>
2014-09-23 17:24:07 +0800

    Adding data mockups

--- web/index.php
+++ web/index.php
@@ -13,15 +13,47 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/views',
 ));
+// Register doctrine database
+
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Herrera\Pdo\PdoServiceProvider(),
+    array(
+        'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"],
+        'pdo.port' => $dbopts["port"],
+        'pdo.username' => $dbopts["user"],
+        'pdo.password' => $dbopts["pass"]
+    )
+);
 
 // Our web handlers
 
 $app->get('/', function() use($app) {
     $app['monolog']->addDebug('logging output.');
-    return $app['twig']->render('index.twig', array(
+    $q = $app['pdo']->prepare("SELECT * FROM books LIMIT 9");
+    $q->execute();
+
+    $books = array();
+    while($row = $q->fetch(PDO::FETCH_ASSOC)){
+        $books[] = $row;
+    }
+    return $app['twig']->render('home.html', array(
         'title' => "Bookmart - Home",
         'carousel' => array('slider1.png' => '#', 'slider2.png' => '#', 'slider3.png' => '#', 'slider4.png' => '#'),
-        'banner' => array('image' => 'banner.png', 'description' => 'Grab a copy of Gillian Flynn\'s global best seller GONE GIRL before its movie adaptation hits the theatres worldwide.', 'link' => '#')
+        'banner' => array('image' => 'banner.png', 'description' => 'Grab a copy of Gillian Flynn\'s global best seller GONE GIRL before its movie adaptation hits the theatres worldwide.', 'link' => '#'),
+        'books' => $books
+    ));
+});
+
+$app->get('/books',function() use($app){
+    $q = $app['pdo']->prepare("SELECT * FROM books");
+    $q->execute();
+
+    $books = array();
+    while($row = $q->fetch(PDO::FETCH_ASSOC)){
+        $books[] = $row;
+    }
+    return $app['twig']->render('books.html',array(
+        'title' => 'Browse Books'
     ));
 });
 
